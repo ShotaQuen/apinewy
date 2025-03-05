41590ad69a59c7e4dc1db4451d893af567f3f295
ShotaQuen <imanadja86@gmail.com>
2025-03-05 08:43:01 +0800

    ikann

--- index.js
+++ index.js
@@ -128,6 +128,26 @@ app.get('/api/npmStalk', async (req, res) => {
   }
 });
 
+<<<<<<< HEAD
+=======
+app.get('/api/ffStalk', async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query parameter 'q' is required" });
+  }
+  try {
+    const { ffStalk } = require('./scrape')
+    const response = await ffStalk.stalk(q);    res.status(200).json({
+      status: true,
+      creator: 'ikann',
+      data: response.data
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message });
+  }
+});
+
+>>>>>>> dee6a1d (Pesan perubahan yang jelas)
 app.get('/api/viooai', async (req, res) => {
   const { q } = req.query;
 
